2c98fa6af206706a6521ca4aefc750c3bb1b28d3
wiidler <weidlerwill@gmail.com>
2022-02-08 12:02:41 -0600

    Progress on HW02
    Wrote initial welcoming phrases

--- wordle.cpp
+++ wordle.cpp
@@ -8,6 +8,29 @@
 #include <iostream>
 using namespace std;
 int main(){
+	cout << "Welcome to Wordle For Friends!" << endl << "≤^.^≥"<< endl << endl;
+
+	cout << "Player 1, please enter your name: ";
+
+	string playerOneName;
+	cin >> playerOneName;
+
+	cout << endl << "Welcome, " << playerOneName << "!" << endl;
+
+	cout << endl << "Now, Player 2, please enter your name: ";
+
+	string playerTwoName;
+	cin >> playerTwoName;
+
+	cout << endl << "Welcome, " << playerTwoName << "!" << endl;
+
+
+
+
+
+
+
+
     string str = "LAUGH"; 
 		// note: the first character in a string is
 		// 	located at index position 0 (not 1) 
